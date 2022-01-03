a3e0e4c413cd2a2787e5cbecd7310671666afe1a
ckatjrdavis <ckatjrdavis@gmail.com>
2022-01-03 14:05:24 -0600

    Added dinosaur class

--- robot.py
+++ robot.py
@@ -5,15 +5,8 @@ chosen_weapon = Weapon()
 class Robot:
     def __init__(self, name):
         self.name = name
+        self.health = 0
+        self.weapon = chosen_weapon
 
     def attack(self, dinosaur):
         pass
-
-    def health(self, robot_hp):
-        self.robot_health = robot_hp
-
-    def weapon(self):
-        weapon = chosen_weapon.__init__('Damocles', 25)
-
-    # def robot_name(self, robot_name):
-    #     self.name = robot_name
--- weapon.py
+++ weapon.py
@@ -1,10 +1,4 @@
 class Weapon:
     def __init__(self, name, attack_power):
         self.weapon_name = name
-        self.attack_pwr = attack_power
-
-    # def name(self):
-    #     name = 'Damocles'
-
-    # def attack(self):
-    #     attack_power = 25
+        self.attack_power = attack_power
