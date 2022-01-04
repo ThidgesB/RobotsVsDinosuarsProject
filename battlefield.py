6cf1549b4b7e44ba6abb2f66d9b1af7683cc4531
ckatjrdavis <ckatjrdavis@gmail.com>
2022-01-04 14:36:41 -0600

    began writing attack function in robot class

--- battlefield.py
+++ battlefield.py
@@ -1,5 +1,12 @@
 from herd import Herd
 from fleet import Fleet
+import random
+
+def random_selection(selection):
+    random_index = random.randint(0,len(selection)-1)
+    result = selection[random_index]
+    #print(result)
+    return result
 
 class Battlefield:
     def __init__(self):
@@ -9,6 +16,7 @@ class Battlefield:
 
     def run_game(self): # can call this to run our other methods weve made
         self.display_welcome()
+        self.robo_turn()
         pass
 
     def display_welcome(self):
@@ -18,16 +26,31 @@ class Battlefield:
         pass
 
     def dino_turn(self, dinosaur): # might include show_diono_opponent_options also remember you have attack logic in class
+        dino_index = random_selection(self.dinosaur_herd.dinos)
         pass
 
-    def robo_turn(self, robot): # might include show_robo_opponent_options  also remember you have attack logic in class
+    def robo_turn(self): # might include show_robo_opponent_options  also remember you have attack logic in class
+        self.show_robo_opponent_options()
+        robot_index = int(input("Select robot to attack with"))
+        self.show_dino_opponent_options()
+        dino_index = int(input("Select dinosaur to attack"))
+        self.robot_fleet.robots[robot_index].attack(self.dinosaur_herd.dinos[dino_index])  #<--,(self.herd.dinos)
         pass
 
     def show_dino_opponent_options(self):
+        print('Current Dinosaur Herd (Enemies)')
+        temp = 0
+        for dino in self.dinosaur_herd.dinos:
+            print(f'Press {temp} to select {dino.name} ({dino.health} HP)')
+            temp += 1
         pass
 
     def show_robo_opponent_options(self):
-        pass
+        print("Current Robot Fleet")
+        temp = 0
+        for robot in self.robot_fleet.robots:
+            print(f'Press {temp} to select {robot.name} ({robot.health} HP)')
+            temp += 1
 
     def display_winners(self):
         pass
--- robot.py
+++ robot.py
@@ -9,5 +9,9 @@ class Robot:
         self.weapon = Weapon()
 
 
-    def attack(self, dinosaur):
+    def attack(self, dino): # dinosaur.name or dinosaur.health   dino_list
+        dino.health = dino.health - self.weapon.weapon_attack_power
+        print(f'Dinosaur {dino.name} has been attacked by robot {self.name} with {self.weapon.weapon_name} and did {self.weapon.weapon_attack_power} damage ') # this is where the math happens to subtract the robot attack power from the dino health
+        print(f'{dino.name} has {dino.health} remaining')
+            # think about adding an if statment "if" dino heath is 0 or less than he can be removed from the list in this step.
         pass
